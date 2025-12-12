ff08899f5dc3d901898e67afc2a334af1d6ab779
Veronika Bukharina <nika_snow@mail.ru>
2025-12-13 02:07:50 +0500

    Create Page Objects for web tests

--- src/test/java/com/example/pages/web/HomePage.java
+++ src/test/java/com/example/pages/web/HomePage.java
@@ -1,4 +1,76 @@
 package com.example.pages.web;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+
 public class HomePage {
+    private WebDriver driver;
+    private WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+    @FindBy(css = "a[data-qa='header-category-menu']")
+    private WebElement catalogLink;
+
+    @FindBy(css = "input[data-name='Search-Input']")
+    private WebElement searchInput;
+
+    @FindBy(css = "button[data-name='Search-Button']")
+    private WebElement searchButton;
+
+    @FindBy(css = "a[href*='/newauto/']")
+    private WebElement newCarsLink;
+
+    @FindBy(css = "a[href*='/used/']")
+    private WebElement usedCarsLink;
+
+    @FindBy(css = "h1[data-name='PageTitle']")
+    private WebElement pageTitle;
+
+    @FindBy(css = ".ListingItem")
+    private WebElement listingItem;
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void navigateToCatalog() {
+        wait.until(ExpectedConditions.elementToBeClickable(catalogLink));
+        catalogLink.click();
+    }
+
+    public void searchCar(String query) {
+        wait.until(ExpectedConditions.visibilityOf(searchInput));
+        searchInput.clear();
+        searchInput.sendKeys(query);
+        searchButton.click();
+    }
+
+    public void navigateToNewCars() {
+        wait.until(ExpectedConditions.elementToBeClickable(newCarsLink));
+        newCarsLink.click();
+    }
+
+    public void navigateToUsedCars() {
+        wait.until(ExpectedConditions.elementToBeClickable(usedCarsLink));
+        usedCarsLink.click();
+    }
+
+    public String getPageTitle() {
+        wait.until(ExpectedConditions.visibilityOf(pageTitle));
+        return pageTitle.getText();
+    }
+
+    public boolean isListingVisible() {
+        try {
+            wait.until(ExpectedConditions.visibilityOf(listingItem));
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
 }
--- src/test/java/com/example/pages/web/SearchResultsPage.java
+++ src/test/java/com/example/pages/web/SearchResultsPage.java
@@ -1,4 +1,45 @@
 package com.example.pages.web;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+import java.util.List;
+
 public class SearchResultsPage {
+    private WebDriver driver;
+    private WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+    @FindBy(css = ".ListingItem")
+    private List<WebElement> resultsList;
+
+    @FindBy(css = "select[data-name='YearFrom']")
+    private WebElement yearFromFilter;
+
+    @FindBy(css = "option[value='2020']")
+    private WebElement yearOption;
+
+    @FindBy(css = "button[data-name='ApplyFilters']")
+    private WebElement applyFiltersButton;
+
+    public SearchResultsPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public int getResultsCount() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(org.openqa.selenium.By.cssSelector(".ListingItem")));
+        return resultsList.size();
+    }
+
+    public void applyYearFilter() {
+        wait.until(ExpectedConditions.elementToBeClickable(yearFromFilter));
+        yearFromFilter.click();
+        yearOption.click();
+        applyFiltersButton.click();
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(org.openqa.selenium.By.cssSelector(".Loader")));
+    }
 }
